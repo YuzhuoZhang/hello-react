af608d7858b30541bcc59dfae765f59449c06d4e
zyz <1415068436@qq.com>
2020-02-02 16:15:20 +0800

    component

---
+++ src/form.js
@@ -0,0 +1,38 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+class NameForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            value:''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            value:event.target.value
+        });
+    }
+
+    handleSubmit(event){
+        alert('提交的名字:' + this.state.value);
+        // event.preventDefault();
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.onSubmit}>
+                <label>
+                    name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="提交" />
+            </form>
+        )
+    }
+}
+export default NameForm;
+
--- src/index.js
+++ src/index.js
@@ -1,21 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css';
+import NameForm from './form';
+import Clock from './clock';
+import LoginControl from './login-control';
 
-function Welcome(props){
-    return <h1>Hello,{props.name}!</h1>
+
+
+
+
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isToggleOn: true
+        };
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+    /**
+     * handleClick里的this已经不是类的this了，解决方案：
+     * 1.手动绑定this：this.handleClick = this.handleClick.bind(this);
+     * 2.写成箭头函数:()=>this.setState(....);
+     */
+
+    handleClick = () => this.setState({ isToggleOn: !this.state.isToggleOn });
+
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+
 }
 
-function tick(){
-    const element = (
-        <div>
-            <Welcome name = 'zyz' />
-            <h1>hello,react!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
+function ListItem(props){
+    return <li>{props.value}</li>;
+}
+
+function NumberList(props){
+    const numbers = props.numbers;
+    const listItems = numbers.map(
+        (number)=>
+        <ListItem key={number.toString()} value={number} />
     );
-    ReactDOM.render(element,document.getElementById('root'));
-
+    return (
+        <ul>{listItems}</ul>
+    )
 }
 
-setInterval(tick,1000);
+function Blog(props) {
+    const sidebar = (
+      <ul>
+        {props.posts.map((post) =>
+          <li key={post.id}>
+            {post.title}
+          </li>
+        )}
+      </ul>
+    );
+    const content = props.posts.map((post) =>
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.content}</p>
+      </div>
+    );
+    return (
+      <div>
+        {sidebar}
+        <hr />
+        {content}
+      </div>
+    );
+  }
+  
+  const posts = [
+    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+    {id: 2, title: 'Installation', content: 'You can install React from npm.'},
+    {id: 3, title: 'finish', content: 'you can explore react by yourself.'}
+  ];
+
+const numbers = [1,2,3];
+ReactDOM.render(
+    <div>
+        <Clock />
+        <Toggle />
+        <LoginControl />
+        <NumberList numbers={numbers} />
+        <Blog posts={posts}/>
+        <NameForm />
+    </div>,
+    document.getElementById('root')
+);
+
