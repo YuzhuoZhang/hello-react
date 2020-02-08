85a106bfdf8422da44abdbf3b17d5670ab2e410f
zyz <1415068436@qq.com>
2020-02-08 11:47:47 +0800

    update

--- src/temperature-calculator.jsx
+++ src/temperature-calculator.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 
+const scaleNames = {
+    c:'Celsius',
+    f:'Fahrenheit'
+};
+
 function BoilingVerdict(props){
-    if(props.celsicus>=100){
+    if(props.celsius>=100){
         return (<p>The water would boil.</p>);
     }
     return (<p>The water would not boil.</p>);
+}
+
+class TemperatureInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state={
+            temperature:''
+        };
+    }
+
+    handleChange(event){
+        this.setState({
+            temperature:event.target.value
+        });
+    }
+
+    render(){
+        const temperature = this.state.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}</legend>
+                <input value={temperature} onChange={this.onChange} />
+            </fieldset>
+        );
+    }
+
 }
 
 class Calculator extends React.Component{
@@ -27,9 +60,9 @@ class Calculator extends React.Component{
 
         return (
         <fieldset>
-            <legend>Enter temperature in Celsicus:</legend>
+            <legend>Enter temperature in Celsius:</legend>
             <input value={temperature} onChange={this.handleChange}/>
-            <BoilingVerdict celsicus = {parseFloat(temperature)} />
+            <BoilingVerdict celsius = {parseFloat(temperature)} />
 
         </fieldset>);
 
