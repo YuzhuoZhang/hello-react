afd194fedae22e53eef6976f16b97c4d35da4be0
zyz <1415068436@qq.com>
2020-02-08 10:16:55 +0800

    format

--- src/form.jsx
+++ src/form.jsx
@@ -1,60 +1,60 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 
-class NameForm extends React.Component{
-    constructor(props){
+class NameForm extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            value:'',
-            textValue:'',
-            selectBoxVar1:'man',
-            isGoing:true,
-            numberOfGuests:2
+        this.state = {
+            value: '',
+            textValue: '',
+            selectBoxVar1: 'man',
+            isGoing: true,
+            numberOfGuests: 2
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleTextChange = this.handleTextChange.bind(this);
         this.handleSelectBoxChange = this.handleSelectBoxChange.bind(this);
-        this.handleInputChange=this.handleInputChange.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    handleChange(event){
+    handleChange(event) {
         this.setState({
-            value:event.target.value
+            value: event.target.value
         });
     }
 
-    handleSubmit(event){
-        alert('提交的名字:' + this.state.value+
-        '\n提交的文章:'+this.state.textValue +
-        '您的性别是'+this.state.selectBoxVar1);
+    handleSubmit(event) {
+        alert('提交的名字:' + this.state.value +
+            '\n提交的文章:' + this.state.textValue +
+            '您的性别是' + this.state.selectBoxVar1);
 
         event.preventDefault();
     }
 
-    handleTextChange(event){
+    handleTextChange(event) {
         this.setState({
-            textValue:event.target.value
+            textValue: event.target.value
         });
     }
 
-    handleSelectBoxChange(event){
+    handleSelectBoxChange(event) {
         this.setState({
-            selectBoxVar1:event.target.value
+            selectBoxVar1: event.target.value
         });
     }
 
-    handleInputChange(event){
+    handleInputChange(event) {
         const target = event.target;
-        const value = target.type === 'checkbox'?target.checked : target.value;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
 
         this.setState({
-            [name]:value
+            [name]: value
         });
     }
 
-    render(){
+    render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
@@ -70,21 +70,21 @@ class NameForm extends React.Component{
                     </select>
                     {/* 文件: */}
                     {/* <input type='file'/> */}
-                    <br/>
+                    <br />
                     <label>
                         参与:
-                        <input name="isGoing" type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange}/>
+                        <input name="isGoing" type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
                     </label>
 
                     <label>
                         来宾人数:
                         <input name="numberOfGuests" type="number" value={this.state.numberOfGuests} onChange={this.handleInputChange} />
                     </label>
-                    
+
                     {this.state.isGoing.toString()}
                     {this.state.numberOfGuests}
-                    <br/><input value='hi'/>
-                    
+                    <br /><input value='hi' />
+
                 </label>
                 <input type="submit" value="提交" />
             </form>
