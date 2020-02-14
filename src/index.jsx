d8fb1ab67115ec3266d0094f8d3e924e5f5bbf14
zyz <1415068436@qq.com>
2020-02-14 15:36:23 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,102 +1,60 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import './index.css';
-import NameForm from './form';
-import Clock from './clock';
-import LoginControl from './login-control';
-import MyList from './my-list';
-import Calculator from './temperature-calculator';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state={
+      title:'',
+      items:''
+    };
+  }
 
-
-
-
-class Toggle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isToggleOn: true
-        };
-        // this.handleClick = this.handleClick.bind(this);
+  handleClick() {
+    var xmlhttp;
+    if (window.ActiveXObject) {
+      // code for IE6, IE5
+      xmlhttp = new window.ActiveXObject("Microsoft.XMLHTTP");
+      
     }
-
-    /**
-     * handleClick里的this已经不是类的this了，解决方案：
-     * 1.手动绑定this：this.handleClick = this.handleClick.bind(this);
-     * 2.写成箭头函数:()=>this.setState(....);
-     */
-
-    handleClick = () => this.setState({ isToggleOn: !this.state.isToggleOn });
-
-
-    render() {
-        return (
-            <button onClick={this.handleClick}>
-                {this.state.isToggleOn ? 'ON' : 'OFF'}
-            </button>
-        );
+    else {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp = new XMLHttpRequest();
     }
+    xmlhttp.open('GET','127.0.0.1:8080/api/getObjects',true);
+    xmlhttp.send();
+    var objects = xmlhttp.responseText;
+    alert(objects);
+  }
 
-}
-
-function ListItem(props){
-    return <li>{props.value}</li>;
-}
-
-function NumberList(props){
-    const numbers = props.numbers;
-    const listItems = numbers.map(
-        (number)=>
-        <ListItem key={number.toString()} value={number} />
-    );
+  render() {
     return (
-        <ul>{listItems}</ul>
-    )
-}
 
-function Blog(props) {
-    const sidebar = (
-      <ul>
-        {props.posts.map((post) =>
-          <li key={post.id}>
-            {post.title}
-          </li>
-        )}
-      </ul>
-    );
-    const content = props.posts.map((post) =>
-      <div key={post.id}>
-        <h3>{post.title}</h3>
-        <p>{post.content}</p>
-      </div>
-    );
-    return (
-      <div>
-        {sidebar}
-        <hr />
-        {content}
-      </div>
+      <React.Fragment>
+        <button onClick={this.handleClick}>向服务器请求数据</button>
+        <table>
+          <th>
+            <TableHeader title={this.state.title}/>
+          </th>
+          <tr>
+            <TableRows rows={this.state.items} />
+          </tr>
+        </table>
+      </React.Fragment>
     );
   }
-  
-  const posts = [
-    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
-    {id: 2, title: 'Installation', content: 'You can install React from npm.'},
-    {id: 3, title: 'finish', content: 'you can explore react by yourself.'}
-  ];
 
-const numbers = [1,2,3];
+}
+
+class TableHeader extends React.Component{
+}
+
+class TableRows extends React.Component{
+
+}
+
 ReactDOM.render(
-    <div>
-        <Clock />
-        {/* <Toggle />
-        <LoginControl />
-        <NumberList numbers={numbers} />
-        <Blog posts={posts}/>
-        <NameForm />
-        <MyList /> */}
-        <Calculator />
-    </div>,
-    document.getElementById('root')
-);
-
+  <App />,
+  document.getElementById('root')
+  );
