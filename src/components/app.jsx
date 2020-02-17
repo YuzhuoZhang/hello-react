50185d5f1b4d34731dcad393252af85fb750ea0d
zyz <1415068436@qq.com>
2020-02-17 20:03:33 +0800

    重新开始

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React,{Componet} from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Componet{
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default App
